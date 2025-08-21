c99d6952aa736c4e918c5e95b147469e4953626f
sarah <sarahlagmay18@gmail.com>
2025-08-21 23:20:54 +0800

    removed unnecessary comment

--- barangayDocumentChecker/script.js
+++ barangayDocumentChecker/script.js
@@ -3,8 +3,6 @@ function login() {
   if (code.trim() === '') {
     alert('Please enter a Barangay Code.');
   } else {
-    // Simulate redirect or auth check
     alert('Logging in with code: ' + code);
-    // You can redirect or do actual authentication here
   }
 }
